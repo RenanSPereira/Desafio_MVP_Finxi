48f502a45715eb811bb0294d8d53fd53be10effc
Renan Santos <renan3santos@gmai.com>
2020-09-07 18:53:59 -0300

    Criação de métodos e Adição de Imports
    "Create": Método possibilita a criação da serialização
    passando um parâmetro adicional.
    Neste caso queriamos pegar o usuário logado e gravar no
    campo anunciante.
    
    "get_queryset": Filtra usuários , se o usuário for o
    administrador ele pode visualizar todas as demandas e se for anunciante
    ele só verá as demandas criadas por ele próprio.

--- api/core/views.py
+++ api/core/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
 from .models import Demanda
-from .serializers import DemandaSerializer
+from .serializers import DemandaSerializer, DemandaCreateSerializer
 
 
 # Create your views here.
@@ -10,3 +11,16 @@ from .serializers import DemandaSerializer
 class DemandaViewSet(viewsets.ModelViewSet):
     queryset = Demanda.objects.all()
     serializer_class = DemandaSerializer
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Demanda.objects.all()
+
+        #mostra as demandas cadastradas pelo usuário logado
+        return Demanda.objects.filter(anunciante=self.request.user.id)
+
+    def create(self, request):
+        serializer = DemandaCreateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        demanda = Demanda.objects.create(**serializer.data, anunciante=request.user)
+        return Response(DemandaSerializer(demanda).data)
