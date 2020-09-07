b788ed39e51aba36c10daf1f6273f8cada0d2a83
Renan Santos <renan3santos@gmai.com>
2020-09-07 17:22:34 -0300

    Registrando Model Demanda no Admin
    Registrando e customizando a Model Demanda

--- api/core/admin.py
+++ api/core/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Demanda
+
 
 # Register your models here.
+
+
+@admin.register(Demanda)
+class DemandaAdmin(admin.ModelAdmin):
+    list_display = (
+        'descricao_peca',
+        'informacao_contato',
+        'status_finalizacao',
+        'anunciante',
+        'data_cadastro',
+    )
+
+    fields = (
+        'descricao_peca',
+        'informacao_contato',
+        'status_finalizacao',
+        'logradouro',
+        'numero',
+        'complemento',
+        'bairro',
+        'cidade',
+        'uf',
+        'cep',
+    )
+
+    def save_model(self, request, obj, form, change):
+        obj.anunciante = request.user
+        super(DemandaAdmin, self).save_model(request, obj, form, change)
